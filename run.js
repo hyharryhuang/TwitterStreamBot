15c64dcbebc813df29e2d089ea793f3dc656cbb8
Harry Huang <hy.harry.huang@gmail.com>
2015-01-06 19:47:10 +0000

    Add summary

--- run.js
+++ run.js
@@ -1,5 +1,6 @@
-var Twit = require('twit')
-var config = require('./config')
+var Twit = require('twit');
+var Gramophone = require('gramophone');
+var config = require('./config');
 
 var T = new Twit({
 	consumer_key: config.consumerKey,
@@ -10,7 +11,7 @@ var T = new Twit({
 
 var currentTweets = [];
 
-var phrases = ["You want some? I'll give it to ya!", "You've got no ground.", "You want some or what?"];
+var phrases = ["You want some? I'll give it to ya!", "You've got no ground.", "You want some or what?", "You wanna deal with me?", "You've got not fans!"];
 var endingPhrase = "I think you mean 'a lot'.";
 var stream = T.stream('statuses/filter', { track: config.trackingWord });
 
@@ -27,6 +28,12 @@ stream.on('tweet', function (tweet) {
 	};
 })
 
+var userStream = T.stream('user');
+
+userStream.on('tweet', function(tweet) {
+	console.log(tweet);
+});
+
 setInterval( function() {
 	if(Object.keys(currentTweets).length)
 	{
@@ -34,10 +41,64 @@ setInterval( function() {
 		var randPhrase = phrases[Math.floor(Math.random() * phrases.length)];
 
 		T.post('statuses/update', { status: '.@' + randTweet.user + ' ' + randPhrase + ' ' + endingPhrase, in_reply_to_status_id: randTweet.replyId }, function(err, data, response) {
-			// console.log('posted status to .@' + randTweet.user + ' ' + randPhrase + " with reply id " + randTweet.replyId);
 		})
 
 		//reset cache
 		currentTweets = [];
 	}
 }, config.minTweetIntervalSeconds * 1000)
+
+function getAndPostSummary() {
+	T.get('statuses/mentions_timeline', {"count":200}, function (err, data, response) {
+		if(data != undefined)
+		{
+			var currentReplies = "";
+			for (var i = 0; i < data.length; i++) {
+				var tweet = data[i]["text"];
+
+				tweet = filterWord(tweet);
+				tweet = filterWord(tweet);
+
+				currentReplies += tweet;
+			}
+
+			var frequentWords = Gramophone.extract(currentReplies, {stopWords:config.stopWords});
+			var summary = constructSummaryTweetWithFrequentWords(frequentWords);
+			console.log(summary);
+
+			T.post('statuses/update', { status: summary}, function(err, data, response) {});
+		}
+	})
+}
+
+function filterWord(word)
+{
+	var withoutUsernames = word.replace(/@([A-Za-z0-9_]+)/, "").trim();
+	var withoutRetweets = withoutUsernames.replace(/RT([A-Za-z0-9_]+)/, "").trim();
+	var withoutQuotes = withoutRetweets.replace(/"(.*?)"/, "").trim();
+
+	return withoutQuotes;
+}
+
+function constructSummaryTweetWithFrequentWords(frequentWords)
+{
+	var summary = config.summaryPrefix;
+
+	var counter = 0;
+
+	for (var i = 0; i < frequentWords.length; i++) {
+		var predicatedSummary = summary + " '" + frequentWords[i] + "'";
+
+		if(predicatedSummary.length < config.twitterLimit)
+		{
+			summary = predicatedSummary;
+		} else
+			break;
+	}
+
+	return summary;
+}
+
+setInterval( function() {
+	getAndPostSummary();
+}, config.summaryIntervalSeconds * 1000)

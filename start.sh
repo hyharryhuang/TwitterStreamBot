e59b4ff83c09762fc73c68435cd8587e78391b79
Harry Huang <hy.harry.huang@gmail.com>
2015-01-06 10:38:02 +0000

    Update startup script

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
 STARTSCRIPT="run.js"
 
 echo "Deploying Wealdstone..."
-sudo pkill -f $STARTSCRIPT
-sudo node $STARTSCRIPT &
+sudo kill -9 `cat wealdstone.pid`
+node $STARTSCRIPT &
+sudo echo $! > wealdstone.pid
